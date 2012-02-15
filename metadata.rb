4d891538b974e3d30801ab7f8e05fb0a8ad59c71
nat <nat.lownes@gmail.com>
2012-02-15 11:02:10 -0500

    doh

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ description      "Installs/Configures postgresql9"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-deps = %(apt)
+deps = %w(apt)
 deps.each do |d|
   depends d
 end
