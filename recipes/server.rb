1480952c6748bf19f5f5d5e3a4c90ab5fbe0d581
nat <nat.lownes@gmail.com>
2012-02-16 18:22:06 -0500

    m

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,14 +1,27 @@
 require 'digest/md5'
 require_recipe 'postgresql9::server_install'
 
+postgres_version = "9.0"
+binaries_path = "/usr/lib/postgresql/#{postgres_version}/bin/"
 db_path = "#{node[:postgresql9][:db_path]}/data"
+
 md5_password = ::Digest::MD5.hexdigest(node[:postgresql9][:password])
+
+execute "halt-postgres" do
+  command "/etc/init.d/postgresql stop"
+  # stop postgres if we've installed/started it
+  # since we'll be changing the data directory 
+  # if it doesn't exist
+  only_if "[ ! -d #{db_path}]"                                      
+end
 
 execute "change-db-ownership-to-postgres" do
   command "chown -R postgres #{db_path}"
   action :nothing
 
   notifies :restart, resources(:service => "postgresql"), :immediately
+
+  notifies :run, resources(:execute => 'create-database-user'), :immediately
 end
                                                                                                                   
 execute "init-postgres" do                                                                                        
@@ -28,13 +41,15 @@ directory node[:postgresql9][:db_path] do
   notifies :run, resources(:execute => "init-postgres"), :immediately
 end
 
-execute "create database user #{node[:postgresql9][:db_user]}" do
+execute "create-database-user" do
   command "createuser -dSR #{node[:postgresql9][:db_user]}"
   user "postgres"
   not_if %{ su postgres -c "psql -c 'SELECT * from pg_roles' |grep -q #{node[:postgresql9][:db_user]}" } 
+
+  notifies :run, resources(:execute => 'set-database-user-password'), :immediately
 end
 
-execute "set database user #{node[:postgresql9][:db_user]} password" do
+execute "set-database-user-password" do
   command %{psql -d postgres -c "ALTER USER #{node[:postgresql9][:db_user]} ENCRYPTED PASSWORD '#{md5_password}'; "}
   user "postgres"
 end
