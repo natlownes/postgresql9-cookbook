558a0ec0d53ea0359c5fd0b60521d61a4a91db9f
nat <nat.lownes@gmail.com>
2012-02-16 18:02:47 -0500

    m

--- recipes/server.rb
+++ recipes/server.rb
@@ -4,6 +4,30 @@ require_recipe 'postgresql9::server_install'
 db_path = "#{node[:postgresql9][:db_path]}/data"
 md5_password = ::Digest::MD5.hexdigest(node[:postgresql9][:password])
 
+execute "change-db-ownership-to-postgres" do
+  command "chown -R postgres #{db_path}"
+  action :nothing
+
+  notifies :restart, resources(:service => "postgresql"), :immediately
+end
+                                                                                                                  
+execute "init-postgres" do                                                                                        
+  command "initdb -D #{db_path} --encoding=UTF8 --locale=en_US.UTF-8"
+  action :nothing                                                                                                   
+  user 'postgres'                                                                                               
+  only_if "[ ! -d #{db_path}]"                                      
+
+  notifies :run, resources(:execute => "change-db-ownership-to-postgres"), :immediately
+end                                                                                                               
+
+directory node[:postgresql9][:db_path] do
+  action :create
+  recursive true
+  owner 'postgres'
+  group 'postgres'
+  notifies :run, resources(:execute => "init-postgres"), :immediately
+end
+
 execute "create database user #{node[:postgresql9][:db_user]}" do
   command "createuser -dSR #{node[:postgresql9][:db_user]}"
   user "postgres"
@@ -29,30 +53,5 @@ end
 execute "ensure postgres ownership of config files" do
   command "chown -R postgres /etc/postgresql"
   notifies :restart,     resources(:service => "postgresql"), :immediately
-end
-
-
-execute "change-db-ownership-to-postgres" do
-  command "chown -R postgres #{db_path}"
-  action :nothing
-
-  notifies :restart, resources(:service => "postgresql"), :immediately
-end
-                                                                                                                  
-execute "init-postgres" do                                                                                        
-  command "initdb -D #{db_path} --encoding=UTF8 --locale=en_US.UTF-8"
-  action :nothing                                                                                                   
-  user 'postgres'                                                                                               
-  only_if "[ ! -d #{db_path}]"                                      
-
-  notifies :run, resources(:execute => "change-db-ownership-to-postgres"), :immediately
-end                                                                                                               
-
-directory node[:postgresql9][:db_path] do
-  action :create
-  recursive true
-  owner 'postgres'
-  group 'postgres'
-  notifies :run, resources(:execute => "init-postgres"), :immediately
 end
 
