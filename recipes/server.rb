d52a442f8211c91dc19ffa7b4d3cc93d04c8d091
nat <nat.lownes@gmail.com>
2012-02-16 14:39:18 -0500

    md5 password

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,3 +1,4 @@
+require 'digest/md5'
 require_recipe 'postgresql9::server_install'
 
 execute "create database user #{node[:postgresql9][:db_user]}" do
@@ -6,8 +7,10 @@ execute "create database user #{node[:postgresql9][:db_user]}" do
   not_if %{ su postgres -c "psql -c 'SELECT * from pg_roles' |grep -q #{node[:postgresql9][:db_user]}" } 
 end
 
+
+md5_password = ::Digest::MD5.hexdigest(node[:postgresql9][:password]
 execute "set database user #{node[:postgresql9][:db_user]} password" do
-  command %{psql -d postgres -c "ALTER USER #{node[:postgresql9][:db_user]} ENCRYPTED PASSWORD '#{node[:postgresql9][:password]}'; "}
+  command %{psql -d postgres -c "ALTER USER #{node[:postgresql9][:db_user]} ENCRYPTED PASSWORD '#{md5_password}'; "}
   user "postgres"
 end
 
