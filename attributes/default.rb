62c60acb80fd142ecf345be79a8ee43b3b57432d
nat <nat.lownes@gmail.com>
2012-03-26 13:51:54 -0400

    changing max_connections to pg default

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,4 +3,4 @@ default[:postgresql9][:db_user] = 'deploy'
 default[:postgresql9][:password] = 'pa$$word'
 default[:postgresql9][:db_path] = "/db/postgresql"
 
-default[:postgresql9][:settings][:max_connections] = 20
+default[:postgresql9][:settings][:max_connections] = 100
